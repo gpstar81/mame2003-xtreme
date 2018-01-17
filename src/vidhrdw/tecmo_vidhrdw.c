1753a1f5e5bd55c3d6088d4ba5de8fac27fcd244
arcadez <35177309+arcadez@users.noreply.github.com>
2018-01-17 22:42:16 +0000

    Video changes needed for Riot and cocktail support

--- src/vidhrdw/tecmo_vidhrdw.c
+++ src/vidhrdw/tecmo_vidhrdw.c
@@ -1,181 +1,215 @@
-/***************************************************************************
+/******************************************************************************
 
-  video hardware for Tecmo games
+  Ganbare Ginkun  (Japan)  (c)1995 TECMO
+  Final StarForce (US)     (c)1992 TECMO
 
-***************************************************************************/
+  Based on sprite drivers from vidhrdw/wc90.c by Ernesto Corvi (ernesto@imagina.com)
 
-#include "driver.h"
-#include "generic.h"
+******************************************************************************/
 
-unsigned char *tecmo_txvideoram,*tecmo_fgvideoram,*tecmo_bgvideoram;
-
-int tecmo_video_type = 0;
-/*
-   video_type is used to distinguish Rygar, Silkworm and Gemini Wing.
-   This is needed because there is a difference in the tile and sprite indexing.
-*/
-
-static struct tilemap *tx_tilemap,*fg_tilemap,*bg_tilemap;
+#include "vidhrdw/generic.h"
 
 
-/***************************************************************************
+data16_t *tecmo16_videoram;
+data16_t *tecmo16_colorram;
+data16_t *tecmo16_videoram2;
+data16_t *tecmo16_colorram2;
+data16_t *tecmo16_charram;
 
-  Callbacks for the TileMap code
+static struct tilemap *fg_tilemap,*bg_tilemap,*tx_tilemap;
+static int flipscreen,game_is_riot;
 
-***************************************************************************/
+/******************************************************************************/
 
-static void get_bg_tile_info(int tile_index)
+static void fg_get_tile_info(int tile_index)
 {
-	unsigned char attr = tecmo_bgvideoram[tile_index+0x200];
+	int tile = tecmo16_videoram[tile_index] & 0x1fff;
+	int color = tecmo16_colorram[tile_index] & 0x0f;
+
 	SET_TILE_INFO(
-			3,
-			tecmo_bgvideoram[tile_index] + ((attr & 0x07) << 8),
-			attr >> 4,
+			1,
+			tile,
+			color,
 			0)
 }
 
-static void get_fg_tile_info(int tile_index)
+static void bg_get_tile_info(int tile_index)
 {
-	unsigned char attr = tecmo_fgvideoram[tile_index+0x200];
+	int tile = tecmo16_videoram2[tile_index] & 0x1fff;
+	int color = (tecmo16_colorram2[tile_index] & 0x0f)+0x10;
+
 	SET_TILE_INFO(
-			2,
-			tecmo_fgvideoram[tile_index] + ((attr & 0x07) << 8),
-			attr >> 4,
+			1,
+			tile,
+			color,
 			0)
 }
 
-static void gemini_get_bg_tile_info(int tile_index)
+static void tx_get_tile_info(int tile_index)
 {
-	unsigned char attr = tecmo_bgvideoram[tile_index+0x200];
-	SET_TILE_INFO(
-			3,
-			tecmo_bgvideoram[tile_index] + ((attr & 0x70) << 4),
-			attr & 0x0f,
-			0)
-}
-
-static void gemini_get_fg_tile_info(int tile_index)
-{
-	unsigned char attr = tecmo_fgvideoram[tile_index+0x200];
-	SET_TILE_INFO(
-			2,
-			tecmo_fgvideoram[tile_index] + ((attr & 0x70) << 4),
-			attr & 0x0f,
-			0)
-}
-
-static void get_tx_tile_info(int tile_index)
-{
-	unsigned char attr = tecmo_txvideoram[tile_index+0x400];
+	int tile = tecmo16_charram[tile_index];
 	SET_TILE_INFO(
 			0,
-			tecmo_txvideoram[tile_index] + ((attr & 0x03) << 8),
-			attr >> 4,
+			tile & 0x0fff,
+			tile >> 12,
 			0)
 }
 
+/******************************************************************************/
 
-
-/***************************************************************************
-
-  Start the video hardware emulation.
-
-***************************************************************************/
-
-VIDEO_START( tecmo )
+VIDEO_START( fstarfrc )
 {
-	if (tecmo_video_type == 2)	/* gemini */
-	{
-		bg_tilemap = tilemap_create(gemini_get_bg_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT,16,16,32,16);
-		fg_tilemap = tilemap_create(gemini_get_fg_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT,16,16,32,16);
-	}
-	else	/* rygar, silkworm */
-	{
-		bg_tilemap = tilemap_create(get_bg_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT,16,16,32,16);
-		fg_tilemap = tilemap_create(get_fg_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT,16,16,32,16);
-	}
-	tx_tilemap = tilemap_create(get_tx_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT, 8, 8,32,32);
+	fg_tilemap = tilemap_create(fg_get_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT,16,16,32,32);
+	bg_tilemap = tilemap_create(bg_get_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT,16,16,32,32);
+	tx_tilemap = tilemap_create(tx_get_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT, 8, 8,64,32);
 
-	if (!bg_tilemap || !fg_tilemap || !tx_tilemap)
+	if (!fg_tilemap || !bg_tilemap || !tx_tilemap)
 		return 1;
 
-	tilemap_set_transparent_pen(bg_tilemap,0);
 	tilemap_set_transparent_pen(fg_tilemap,0);
+	tilemap_set_transparent_pen(bg_tilemap,0);
 	tilemap_set_transparent_pen(tx_tilemap,0);
 
-	tilemap_set_scrolldx(bg_tilemap,-48,256+48);
-	tilemap_set_scrolldx(fg_tilemap,-48,256+48);
+	tilemap_set_scrolly(tx_tilemap,0,-16);
+	flipscreen = 0;
+	game_is_riot = 0;
+	return 0;
+}
+
+VIDEO_START( ginkun )
+{
+	fg_tilemap = tilemap_create(fg_get_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT,16,16,64,32);
+	bg_tilemap = tilemap_create(bg_get_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT,16,16,64,32);
+	tx_tilemap = tilemap_create(tx_get_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT, 8, 8,64,32);
+
+	if (!fg_tilemap || !bg_tilemap || !tx_tilemap)
+		return 1;
+
+	tilemap_set_transparent_pen(fg_tilemap,0);
+	tilemap_set_transparent_pen(bg_tilemap,0);
+	tilemap_set_transparent_pen(tx_tilemap,0);
+	flipscreen = 0;
+	game_is_riot = 0;
+
+	return 0;
+}
+
+VIDEO_START( riot )
+{
+	fg_tilemap = tilemap_create(fg_get_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT,16,16,64,32);
+	bg_tilemap = tilemap_create(bg_get_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT,16,16,64,32);
+	tx_tilemap = tilemap_create(tx_get_tile_info,tilemap_scan_rows,TILEMAP_TRANSPARENT, 8, 8,64,32);
+
+	if (!fg_tilemap || !bg_tilemap || !tx_tilemap)
+		return 1;
+
+	tilemap_set_transparent_pen(fg_tilemap,0);
+	tilemap_set_transparent_pen(bg_tilemap,0);
+	tilemap_set_transparent_pen(tx_tilemap,0);
+
+	tilemap_set_scrolldy(tx_tilemap,-16,-16);
+
+	flipscreen = 0;
+	game_is_riot = 1;
 
 	return 0;
 }
 
 
+/******************************************************************************/
 
-/***************************************************************************
-
-  Memory handlers
-
-***************************************************************************/
-
-WRITE_HANDLER( tecmo_txvideoram_w )
+WRITE16_HANDLER( tecmo16_videoram_w )
 {
-	if (tecmo_txvideoram[offset] != data)
-	{
-		tecmo_txvideoram[offset] = data;
-		tilemap_mark_tile_dirty(tx_tilemap,offset & 0x3ff);
-	}
+	int oldword = tecmo16_videoram[offset];
+	COMBINE_DATA(&tecmo16_videoram[offset]);
+	if (oldword != tecmo16_videoram[offset])
+		tilemap_mark_tile_dirty(fg_tilemap,offset);
 }
 
-WRITE_HANDLER( tecmo_fgvideoram_w )
+WRITE16_HANDLER( tecmo16_colorram_w )
 {
-	if (tecmo_fgvideoram[offset] != data)
-	{
-		tecmo_fgvideoram[offset] = data;
-		tilemap_mark_tile_dirty(fg_tilemap,offset & 0x1ff);
-	}
+	int oldword = tecmo16_colorram[offset];
+	COMBINE_DATA(&tecmo16_colorram[offset]);
+	if (oldword != tecmo16_colorram[offset])
+		tilemap_mark_tile_dirty(fg_tilemap,offset);
 }
 
-WRITE_HANDLER( tecmo_bgvideoram_w )
+WRITE16_HANDLER( tecmo16_videoram2_w )
 {
-	if (tecmo_bgvideoram[offset] != data)
-	{
-		tecmo_bgvideoram[offset] = data;
-		tilemap_mark_tile_dirty(bg_tilemap,offset & 0x1ff);
-	}
+	int oldword = tecmo16_videoram2[offset];
+	COMBINE_DATA(&tecmo16_videoram2[offset]);
+	if (oldword != tecmo16_videoram2[offset])
+		tilemap_mark_tile_dirty(bg_tilemap,offset);
 }
 
-WRITE_HANDLER( tecmo_fgscroll_w )
+WRITE16_HANDLER( tecmo16_colorram2_w )
 {
-	static UINT8 scroll[3];
-
-	scroll[offset] = data;
-
-	tilemap_set_scrollx(fg_tilemap,0,scroll[0] + 256 * scroll[1]);
-	tilemap_set_scrolly(fg_tilemap,0,scroll[2]);
-}
-
-WRITE_HANDLER( tecmo_bgscroll_w )
-{
-	static UINT8 scroll[3];
-
-	scroll[offset] = data;
-
-	tilemap_set_scrollx(bg_tilemap,0,scroll[0] + 256 * scroll[1]);
-	tilemap_set_scrolly(bg_tilemap,0,scroll[2]);
-}
-
-WRITE_HANDLER( tecmo_flipscreen_w )
-{
-	flip_screen_set(data & 1);
+	int oldword = tecmo16_colorram2[offset];
+	COMBINE_DATA(&tecmo16_colorram2[offset]);
+	if (oldword != tecmo16_colorram2[offset])
+		tilemap_mark_tile_dirty(bg_tilemap,offset);
 }
 
 
+WRITE16_HANDLER( tecmo16_charram_w )
+{
+	int oldword = tecmo16_charram[offset];
+	COMBINE_DATA(&tecmo16_charram[offset]);
+	if (oldword != tecmo16_charram[offset])
+		tilemap_mark_tile_dirty(tx_tilemap,offset);
+}
 
-/***************************************************************************
+WRITE16_HANDLER( tecmo16_flipscreen_w )
+{
+	flipscreen = data & 0x01;
+	flip_screen_set(flipscreen);
+}
 
-  Display refresh
+/******************************************************************************/
 
-***************************************************************************/
+WRITE16_HANDLER( tecmo16_scroll_x_w )
+{
+	static data16_t scroll;
+	COMBINE_DATA(&scroll);
+	tilemap_set_scrollx(fg_tilemap,0,scroll);
+}
+
+WRITE16_HANDLER( tecmo16_scroll_y_w )
+{
+	static data16_t scroll;
+	COMBINE_DATA(&scroll);
+	tilemap_set_scrolly(fg_tilemap,0,scroll);
+}
+
+WRITE16_HANDLER( tecmo16_scroll2_x_w )
+{
+	static data16_t scroll;
+	COMBINE_DATA(&scroll);
+	tilemap_set_scrollx(bg_tilemap,0,scroll);
+}
+
+WRITE16_HANDLER( tecmo16_scroll2_y_w )
+{
+	static data16_t scroll;
+	COMBINE_DATA(&scroll);
+	tilemap_set_scrolly(bg_tilemap,0,scroll);
+}
+
+WRITE16_HANDLER( tecmo16_scroll_char_x_w )
+{
+	static data16_t scroll;
+	COMBINE_DATA(&scroll);
+	tilemap_set_scrollx(tx_tilemap,0,scroll);
+}
+
+WRITE16_HANDLER( tecmo16_scroll_char_y_w )
+{
+	static data16_t scroll;
+	COMBINE_DATA(&scroll);
+	tilemap_set_scrolly(tx_tilemap,0,scroll-16);
+}
+
+/******************************************************************************/
 
 static void draw_sprites(struct mame_bitmap *bitmap,const struct rectangle *cliprect)
 {
@@ -192,37 +226,37 @@ static void draw_sprites(struct mame_bitmap *bitmap,const struct rectangle *clip
 		{42,43,46,47,58,59,62,63}
 	};
 
-	for (offs = spriteram_size-8;offs >= 0;offs -= 8)
+	for (offs = spriteram_size/2 - 8;offs >= 0;offs -= 8)
 	{
-		int flags = spriteram[offs+3];
-		int priority = flags>>6;
-		int bank = spriteram[offs+0];
-		if (bank & 4)
-		{ /* visible */
-			int which = spriteram[offs+1];
-			int code,xpos,ypos,flipx,flipy,priority_mask,x,y;
-			int size = spriteram[offs + 2] & 3;
+		if (spriteram16[offs] & 0x04)	/* enable */
+		{
+			int code,color,sizex,sizey,flipx,flipy,xpos,ypos;
+			int x,y,priority,priority_mask;
 
-			if (tecmo_video_type != 0)	/* gemini, silkworm */
-			  code = which + ((bank & 0xf8) << 5);
-			else						/* rygar */
-			  code = which + ((bank & 0xf0) << 4);
+			code = spriteram16[offs+1];
+			color = (spriteram16[offs+2] & 0xf0) >> 4;
+			sizex = 1 << ((spriteram16[offs+2] & 0x03) >> 0);
+			sizey = 1 << ((spriteram16[offs+2] & 0x0c) >> 2);
 
-			code &= ~((1 << (size*2)) - 1);
-			size = 1 << size;
+			if(game_is_riot)
+				sizey = sizex;
+			else
+				sizey = 1 << ((spriteram16[offs+2] & 0x0c) >> 2);
 
-			xpos = spriteram[offs + 5] - ((flags & 0x10) << 4);
-			ypos = spriteram[offs + 4] - ((flags & 0x20) << 3);
-			flipx = bank & 1;
-			flipy = bank & 2;
 
-			if (flip_screen)
-			{
-				xpos = 256 - (8 * size) - xpos;
-				ypos = 256 - (8 * size) - ypos;
-				flipx = !flipx;
-				flipy = !flipy;
-			}
+			if (sizex >= 2) code &= ~0x01;
+			if (sizey >= 2) code &= ~0x02;
+			if (sizex >= 4) code &= ~0x04;
+			if (sizey >= 4) code &= ~0x08;
+			if (sizex >= 8) code &= ~0x10;
+			if (sizey >= 8) code &= ~0x20;
+			flipx = spriteram16[offs] & 0x01;
+			flipy = spriteram16[offs] & 0x02;
+			xpos = spriteram16[offs+4];
+			if (xpos >= 0x8000) xpos -= 0x10000;
+			ypos = spriteram16[offs+3];
+			if (ypos >= 0x8000) ypos -= 0x10000;
+			priority = (spriteram16[offs] & 0xc0) >> 6;
 
 			/* bg: 1; fg:2; text: 4 */
 			switch (priority)
@@ -234,15 +268,29 @@ static void draw_sprites(struct mame_bitmap *bitmap,const struct rectangle *clip
 				case 0x3: priority_mask = 0xf0|0xcc|0xaa; break; /* obscured by bg and fg */
 			}
 
-			for (y = 0;y < size;y++)
+			if (flipscreen)
 			{
-				for (x = 0;x < size;x++)
+				flipx = !flipx;
+				flipy = !flipy;
+			}
+
+			for (y = 0;y < sizey;y++)
+			{
+				for (x = 0;x < sizex;x++)
 				{
-					int sx = xpos + 8*(flipx?(size-1-x):x);
-					int sy = ypos + 8*(flipy?(size-1-y):y);
-					pdrawgfx(bitmap,Machine->gfx[1],
+					int sx,sy;
+
+					if (!flipscreen)
+					{
+						sx = xpos + 8*(flipx?(sizex-1-x):x);
+						sy = ypos + 8*(flipy?(sizey-1-y):y);
+					} else {
+						sx = 256 - (xpos + 8*(!flipx?(sizex-1-x):x) + 8);
+						sy = 256 - (ypos + 8*(!flipy?(sizey-1-y):y) + 8);
+					}
+					pdrawgfx(bitmap,Machine->gfx[2],
 							code + layout[y][x],
-							flags & 0xf,
+							color,
 							flipx,flipy,
 							sx,sy,
 							cliprect,TRANSPARENCY_PEN,0,
@@ -253,11 +301,12 @@ static void draw_sprites(struct mame_bitmap *bitmap,const struct rectangle *clip
 	}
 }
 
+/******************************************************************************/
 
-VIDEO_UPDATE( tecmo )
+VIDEO_UPDATE( tecmo16 )
 {
 	fillbitmap(priority_bitmap,0,cliprect);
-	fillbitmap(bitmap,Machine->pens[0x100],cliprect);
+	fillbitmap(bitmap,Machine->pens[0x300],cliprect);
 	tilemap_draw(bitmap,cliprect,bg_tilemap,0,1);
 	tilemap_draw(bitmap,cliprect,fg_tilemap,0,2);
 	tilemap_draw(bitmap,cliprect,tx_tilemap,0,4);
