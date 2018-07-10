9dc0701d1512b70e1a7c6ff595492746e78fa035
Michael <michael.rajotte@gmail.com>
2018-07-10 10:36:18 -0700

    memory limit set

--- src/common.h
+++ src/common.h
@@ -99,7 +99,7 @@ struct GameSamples
 int f_load_sample_sizes;
 
 #define	GAME_SAMPLE_LARGE		2000000 // 2MB
-#define GAME_SAMPLE_MAX_TOTAL	150000000 // 150 MB
+#define GAME_SAMPLE_MAX_TOTAL	90000000 // 90 MB
 
 /***************************************************************************
 
